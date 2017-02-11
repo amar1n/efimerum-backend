e2c47dd5ca4c2260aac4e92f6a5732f887b513ae
Alberto Marín García <alberto.marin@gmail.com>
2017-02-11 20:53:21 +0100

    Nueva estructura de Firebase

--- routes/api/v1/photos.js
+++ routes/api/v1/photos.js
@@ -13,15 +13,48 @@ var vision = require('./../../../lib/googleCloudPlatform').vision;
 var multer = require('./../../../lib/utils').multer;
 var moment = require('moment');
 
+const nodePhotosRequestedByUsers = 'nodePhotosRequestedByUsers';
+const nodePhotos = 'photos';
+const nodeLabels = 'labels';
+const nodePhotosByLabels = 'photosByLabels';
+const nodePhotosPostedByUsers = 'photosPostedByUsers';
+const languageEN = 'EN';
+const efimerumStorageBucket = 'efimerum-photos';
+const efimerumStorageBucketPublicURL = 'https://storage.googleapis.com/efimerum-photos';
+
 router.get('/json', function (req, res) {
-    var user = firebase.auth().currentUser;
-    if (user) {
-        console.log('User is signed in', user);
-    } else {
-        console.log('No user is signed in');
+    var validReqQuery = [
+        'uid',
+        'label',
+        'latitude',
+        'longitude',
+        'distance',
+        'mostViewed',
+        'closeToDying',
+        'longestPhotos',
+        'myPhotos',
+        'lang'];
+
+    var uid = req.query.uid || 'CCq864Wku8damv2C5TaZs4s5cpz2';
+    var label = req.query.label;
+    var latitude = req.query.latitude;
+    var longitude = req.query.longitude;
+    var distance = parseInt(req.query.distance) || 0;
+    var mostViewed = req.query.mostViewed;
+    var closeToDying = req.query.closeToDying;
+    var longestPhotos = req.query.longestPhotos;
+    var myPhotos = req.query.myPhotos;
+    var lang = req.query.lang;
+
+    // Validamos que sólo se reciben los query acordados...
+    var queryKeys = Object.keys(req.query);
+    for (var j = 0; j < queryKeys.length; j++) {
+        if (validReqQuery.indexOf(queryKeys[j]) === -1) {
+            return res.status(400).json({success: false, error: 'Wrong API call (query)'});
+        }
     }
 
-    var photosRef = rootRef.child('photos');
+    var photosRef = rootRef.child(nodePhotos);
     photosRef.once('value')
         .then(function (snap) {
             var snapShuffled = shuffleFirebaseSnapshot(snap);
@@ -33,24 +66,46 @@ router.get('/json', function (req, res) {
 });
 
 router.get('/', function (req, res) {
-    var user = firebase.auth().currentUser;
-    var userKey = 'CCq864Wku8damv2C5TaZs4s5cpz2';
-    if (user) {
-        userKey = user.uid;
-        console.log('User is signed in', user, userKey);
-    } else {
-        console.log('No user is signed in... so uid will be ' + userKey);
+    var validReqQuery = [
+        'uid',
+        'label',
+        'latitude',
+        'longitude',
+        'distance',
+        'mostViewed',
+        'closeToDying',
+        'longestPhotos',
+        'myPhotos',
+        'lang'];
+
+    var uid = req.query.uid || 'CCq864Wku8damv2C5TaZs4s5cpz2';
+    var label = req.query.label;
+    var latitude = req.query.latitude;
+    var longitude = req.query.longitude;
+    var distance = parseInt(req.query.distance) || 0;
+    var mostViewed = req.query.mostViewed;
+    var closeToDying = req.query.closeToDying;
+    var longestPhotos = req.query.longestPhotos;
+    var myPhotos = req.query.myPhotos;
+    var lang = req.query.lang;
+
+    // Validamos que sólo se reciben los query acordados...
+    var queryKeys = Object.keys(req.query);
+    for (var j = 0; j < queryKeys.length; j++) {
+        if (validReqQuery.indexOf(queryKeys[j]) === -1) {
+            return res.status(400).json({success: false, error: 'Wrong API call (query)'});
+        }
     }
 
-    var photosRef = rootRef.child('photos');
+    var photosRef = rootRef.child(nodePhotos);
     photosRef.once('value')
         .then(function (snap) {
             var snapShuffled = shuffleFirebaseSnapshot(snap);
             var updates = {};
-            updates['photos_by_users/' + userKey] = snapShuffled;
+            updates[nodePhotosRequestedByUsers + '/' + uid] = snapShuffled;
             rootRef.update(updates)
                 .then(function () {
-                    return res.status(200).json({success: true, data: rootRef.toString() + 'photos_by_users/' + userKey });
+                    return res.status(200).json({success: true, data: rootRef.toString() + nodePhotosRequestedByUsers + '/' + uid });
                 }).catch(function (error) {
                     return res.status(500).json({success: false, error: error});
                 });
@@ -61,7 +116,30 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', multer.any(), function (req, res) {
-    const fotosBucket = storage.bucket('efimerum-photos');
+    var validReqQuery = [
+        'uid',
+        'latitude',
+        'longitude'];
+
+    var uid = req.query.uid || 'CCq864Wku8damv2C5TaZs4s5cpz2';
+    var latitude = req.query.latitude || 41.375395;
+    var longitude = req.query.longitude || 2.170624;
+
+    // Validamos que se reciben parámetros...
+    if (Object.keys(req.params).length) {
+        debug(config.errors.error400, 'GET/', 'Wrong API call (params)', req.params);
+        return res.status(400).json({success: false, error: 'Wrong API call (params)'});
+    }
+
+    // Validamos que sólo se reciben los query acordados...
+    var queryKeys = Object.keys(req.query);
+    for (var j = 0; j < queryKeys.length; j++) {
+        if (validReqQuery.indexOf(queryKeys[j]) === -1) {
+            return res.status(400).json({success: false, error: 'Wrong API call (query)'});
+        }
+    }
+
+    const fotosBucket = storage.bucket(efimerumStorageBucket);
     var fotoPath = req.files[0].path;
 
     var options = {
@@ -82,27 +160,39 @@ router.post('/', multer.any(), function (req, res) {
                 return res.status(500).json({success: false, error: 'Inappropriate content'});
             } else {
                 var labels = {};
+                var labelsEN = {};
                 var updates = {};
                 detections.labels.forEach(function (label) {
                     if (label.score > 75) {
-                        labels[label.desc] = true;
-                        updates['labels/' + label.desc] = true;
+                        labelsEN[label.desc] = true;
+                        updates[nodeLabels + '/' + languageEN + '/' + label.desc] = true;
                     }
                 });
+                labels[languageEN] = labelsEN;
                 fotosBucket.upload(fotoPath, function (err, file) {
                     if (!err) {
-                        var photoKey = rootRef.child('photos').push().key;
+                        var photoKey = rootRef.child(nodePhotos).push().key;
+                        var imageData = {};
+                        imageData['url'] = efimerumStorageBucketPublicURL + '/' + req.files[0].filename;
+                        imageData['width'] = 0;
+                        imageData['height'] = 0;
+                        var thumbnailData = {};
+                        thumbnailData['url'] = efimerumStorageBucketPublicURL + '/' + req.files[0].filename;
+                        thumbnailData['width'] = 0;
+                        thumbnailData['height'] = 0;
                         var photoData = {
-                            originalName: req.files[0].originalname,
                             creationDate: moment().format(),
-                            fileName: req.files[0].filename,
                             labels: labels,
-                            url: 'https://storage.googleapis.com/efimerum-photos/' + req.files[0].filename
+                            imageData: imageData,
+                            thumbnailData: thumbnailData,
+                            latitude: latitude,
+                            longitude: longitude
                         };
-                        updates['photos/' + photoKey] = photoData;
-                        Object.keys(labels).forEach(function (label) {
-                            updates['photos_by_labels/' + label + '/' + photoKey] = photoData;
+                        updates[nodePhotos + '/' + photoKey] = photoData;
+                        Object.keys(labelsEN).forEach(function (label) {
+                            updates[nodePhotosByLabels + '/' + languageEN + '/' + label + '/' + photoKey] = photoData;
                         });
+                        updates[nodePhotosPostedByUsers + '/' + uid + '/' + photoKey] = photoData;
                         rootRef.update(updates)
                             .then(function () {
                                 fs.unlinkSync(fotoPath);
@@ -121,44 +211,6 @@ router.post('/', multer.any(), function (req, res) {
         }
     });
 });
-
-router.delete('/', function (req, res) {
-    var fotoFirebaseID = req.query.fotoFirebaseID;
-    if (fotoFirebaseID == null) {
-        return res.status(400).json({success: false, error: 'Wrong API call (query)'});
-    }
-
-    var photosRef = rootRef.child('photos');
-    var photoRef = photosRef.child(fotoFirebaseID);
-    photoRef.once('value')
-        .then(function (snap) {
-            // Borrar del Storage
-            var fotosBucket = storage.bucket('efimerum-photos');
-            var photoNameInStorage = snap.val().fileName;
-            var foto = fotosBucket.file(photoNameInStorage);
-            foto.delete()
-                .then(function () {
-                    // Borrar de la BBDD
-                    photoRef.remove(function (error) {
-                        if (!error) {
-                            return res.status(200).json({
-                                success: true,
-                                data: {fotoFirebaseID: fotoFirebaseID, photoNameInStorage: photoNameInStorage}
-                            });
-                        } else {
-                            return res.status(500).json({success: false, error: error});
-                        }
-                    });
-                })
-                .catch(function (err) {
-                    return res.status(500).json({success: false, error: err});
-                });
-        })
-        .catch(function (error) {
-            return res.status(500).json({success: false, error: error});
-        });
-});
-
 
 // --------------------------------------
 // --------------------------------------
