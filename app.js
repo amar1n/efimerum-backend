14148981d1f20727110242680aa6656b2023e088
Alejandro M. Alberto <alex.23p10@gmail.com>
2017-02-14 13:26:59 +0000

    Modified error handlers

--- app.js
+++ app.js
@@ -42,9 +42,10 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    res.json({
+        message: err.message,
+        error: err,
+        status: err.status
     });
   });
 }
@@ -53,9 +54,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+  res.json({
+      message: err.message,
+      error: {},
+      status: err.status
   });
 });
 
