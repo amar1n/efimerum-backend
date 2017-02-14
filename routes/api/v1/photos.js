5e5646547b8adc2f7f550e4acebf99aeb50fc784
Alberto Marín García <alberto.marin@gmail.com>
2017-02-14 01:42:40 +0100

    Se añade la creación del thumbnail

--- routes/api/v1/photos.js
+++ routes/api/v1/photos.js
@@ -13,6 +13,7 @@ var vision = require('./../../../lib/googleCloudPlatform').vision;
 var translate = require('./../../../lib/googleCloudPlatform.js').translate;
 var multer = require('./../../../lib/utils').multer;
 var moment = require('moment');
+var lwip = require('lwip');
 
 const nodePhotosRequestedByUsers = 'nodePhotosRequestedByUsers';
 const nodePhotos = 'photos';
@@ -133,10 +134,13 @@ router.get('/', function (req, res) {
             updates[nodePhotosRequestedByUsers + '/' + uid] = snapShuffled;
             rootRef.update(updates)
                 .then(function () {
-                    return res.status(200).json({success: true, data: rootRef.toString() + nodePhotosRequestedByUsers + '/' + uid });
+                    return res.status(200).json({
+                        success: true,
+                        data: rootRef.toString() + nodePhotosRequestedByUsers + '/' + uid
+                    });
                 }).catch(function (error) {
-                    return res.status(500).json({success: false, error: error});
-                });
+                return res.status(500).json({success: false, error: error});
+            });
         })
         .catch(function (error) {
             return res.status(500).json({success: false, error: error});
@@ -166,14 +170,21 @@ router.get('/', function (req, res) {
  *    "error": "Wrong API call (query)"
  *  }
  */
+/*
+ 1) Realizamos el SafeSearch y la detección de etiquetas
+ 2) Realizamos la traducción de las etiquetas que superen un umbral de 75 puntos
+ 3) Subimos la imagen al storage
+ 4) Creamos el thumbnail de la imagen
+ 5) Subimos el thumbnail de la imagen al storage
+ */
 router.post('/', multer.any(), function (req, res) {
     var validReqQuery = [
         'uid',
         'latitude',
         'longitude'];
 
-    var uid = req.query.uid || 'CCq864Wku8damv2C5TaZs4s5cpz2';
-    var latitude = req.query.latitude || 41.375395;
+    var uid = req.query.uid || 'CCq864Wku8damv2C5TaZs4s5cpz2'; // TODO: eliminar esto cuando se consiga la autenticación
+    var latitude = req.query.latitude || 41.375395; // TODO: qué se hace si no viene info de geolocalización???
     var longitude = req.query.longitude || 2.170624;
 
     // Validamos que se reciben parámetros...
@@ -190,93 +201,157 @@ router.post('/', multer.any(), function (req, res) {
         }
     }
 
-    const fotosBucket = storage.bucket(efimerumStorageBucket);
-    var fotoPath = req.files[0].path;
+    var photoPath = req.files[0].path;
+    var photoFilename = req.files[0].filename;
+    console.log('image', photoPath, photoFilename);
 
+    // 1) Realizamos el SafeSearch y la detección de etiquetas
     var options = {
         maxResults: 100,
         types: ['labels', 'safeSearch'],
         verbose: true
     };
-    vision.detect(fotoPath, options, function (err, detections, apiResponse) {
+    vision.detect(photoPath, options, function (err, detections, apiResponse) {
         if (err) {
-            fs.unlinkSync(fotoPath);
-            res.end('Cloud Vision Error');
-        } else {
-            if (detections.safeSearch.adult == 'LIKELY' || detections.safeSearch.adult == 'VERY_LIKELY' ||
-                detections.safeSearch.spoof == 'LIKELY' || detections.safeSearch.spoof == 'VERY_LIKELY' ||
-                detections.safeSearch.medical == 'LIKELY' || detections.safeSearch.medical == 'VERY_LIKELY' ||
-                detections.safeSearch.violence == 'LIKELY' || detections.safeSearch.violence == 'VERY_LIKELY') {
-                fs.unlinkSync(fotoPath);
-                return res.status(500).json({success: false, error: 'Inappropriate content'});
-            } else {
-                var labels = {};
-                var labelsEN = {};
-                var labelsES = {};
-                var updates = {};
-                detections.labels.forEach(function (label) {
-                    if (label.score > 75) {
-                        labelsEN[label.desc] = true;
-                        updates[nodeLabels + '/' + languageEN + '/' + label.desc] = true;
-
-                        // Translates the labels into Spanish
-                        translate.translate(label.desc, 'es').then(function (results) {
-                            labelsES[results[0]] = true;
-                            updates[nodeLabels + '/' + languageES + '/' + results[0]] = true;
-                        }).catch(function (error) {
-                            console.log('Error trying to translate the label[' + label.desc + '] to spanish: ' + error);
-                        });
-                    }
-                });
-                labels[languageEN] = labelsEN;
-                labels[languageES] = labelsES;
-                fotosBucket.upload(fotoPath, function (err, file) {
-                    if (!err) {
-                        var photoKey = rootRef.child(nodePhotos).push().key;
-                        var imageData = {};
-                        imageData['url'] = efimerumStorageBucketPublicURL + '/' + req.files[0].filename;
-                        imageData['width'] = 0;
-                        imageData['height'] = 0;
-                        var thumbnailData = {};
-                        thumbnailData['url'] = efimerumStorageBucketPublicURL + '/' + req.files[0].filename;
-                        thumbnailData['width'] = 0;
-                        thumbnailData['height'] = 0;
-
-                        var now = moment();
-                        var photoData = {
-                            creationDate: now.unix(),
-                            expirationDate: now.add(1, 'hours').unix(),
-                            labels: labels,
-                            imageData: imageData,
-                            thumbnailData: thumbnailData,
-                            latitude: latitude,
-                            longitude: longitude,
-                            numOfLikes: 0
-                        };
-                        updates[nodePhotos + '/' + photoKey] = photoData;
-                        Object.keys(labelsEN).forEach(function (label) {
-                            updates[nodePhotosByLabels + '/' + languageEN + '/' + label + '/' + photoKey] = photoData;
-                        });
-                        Object.keys(labelsES).forEach(function (label) {
-                            updates[nodePhotosByLabels + '/' + languageES + '/' + label + '/' + photoKey] = photoData;
-                        });
-                        updates[nodePhotosPostedByUsers + '/' + uid + '/' + photoKey] = photoData;
-                        rootRef.update(updates)
-                            .then(function () {
-                                fs.unlinkSync(fotoPath);
-                                return res.status(200).json({success: true, data: photoKey});
-                            }).catch(function (error) {
-                                fs.unlinkSync(fotoPath);
-                                fotosBucket.delete(req.files[0].originalname);
-                                return res.status(500).json({success: false, error: error});
-                        });
-                    } else {
-                        fs.unlinkSync(fotoPath);
-                        return res.status(500).json({success: false, error: err});
-                    }
-                });
-            }
+            console.log('Cloud Vision Error', err);
+            fs.unlinkSync(photoPath);
+            return res.status(500).json({success: false, error: 'Cloud Vision Error'});
         }
+
+        if (detections.safeSearch.adult == 'LIKELY' || detections.safeSearch.adult == 'VERY_LIKELY' ||
+            detections.safeSearch.spoof == 'LIKELY' || detections.safeSearch.spoof == 'VERY_LIKELY' ||
+            detections.safeSearch.medical == 'LIKELY' || detections.safeSearch.medical == 'VERY_LIKELY' ||
+            detections.safeSearch.violence == 'LIKELY' || detections.safeSearch.violence == 'VERY_LIKELY') {
+            console.log('Inappropriate content');
+            fs.unlinkSync(photoPath);
+            return res.status(500).json({success: false, error: 'Inappropriate content'});
+        }
+
+        // 2) Realizamos la traducción de las etiquetas que superen un umbral de 75 puntos
+        var labels = {};
+        var labelsEN = {};
+        var labelsES = {};
+        var updates = {};
+        detections.labels.forEach(function (label) {
+            if (label.score > 75) {
+                labelsEN[label.desc] = true;
+                updates[nodeLabels + '/' + languageEN + '/' + label.desc] = true;
+
+                // Translates the labels into Spanish
+                translate.translate(label.desc, 'es')
+                    .then(function (results) {
+                        labelsES[results[0]] = true;
+                        updates[nodeLabels + '/' + languageES + '/' + results[0]] = true;
+                    })
+                    .catch(function (error) {
+                        console.log('Error trying to translate the label[' + label.desc + '] to spanish: ' + error);
+                    });
+            }
+        });
+        labels[languageEN] = labelsEN;
+        labels[languageES] = labelsES;
+
+        // 3) Subimos la imagen al storage
+        const fotosBucket = storage.bucket(efimerumStorageBucket);
+        fotosBucket.upload(photoPath, function (err, file) {
+            if (!err) {
+                console.log('Error storing the image in Google Cloud Storage', err);
+                fs.unlinkSync(photoPath);
+                return res.status(500).json({success: false, error: err});
+            }
+
+            // 4) Creamos el thumbnail de la imagen
+            lwip.open(photoPath, function (err, image) {
+                if (err) {
+                    console.log('Error when open the image!', err);
+                    fs.unlinkSync(photoPath);
+                    fotosBucket.delete(photoFilename);
+                    return res.status(500).json({success: false, error: 'Error when open the image!'});
+                }
+                image.scale(0.35, function (err, image) {
+                    if (err) {
+                        console.log('Error when scale the image!', err);
+                        fs.unlinkSync(photoPath);
+                        fotosBucket.delete(photoFilename);
+                        return res.status(500).json({success: false, error: 'Error when scale the image!'});
+                    }
+                    var photoSplitedByDot = photoPath.split('.');
+                    var thumbnailPath = photoSplitedByDot[0] + '_thumb.' + photoSplitedByDot[photoSplitedByDot.length - 1];
+                    var thumbnailSplitedBySlash = thumbnailPath.split('/');
+                    var thumbnailFilename = thumbnailSplitedBySlash[thumbnailSplitedBySlash.length - 1];
+                    image.writeFile(thumbnailPath, function (err) {
+                        if (err) {
+                            console.log('Error when save the scaled image!', err);
+                            fs.unlinkSync(photoPath);
+                            fotosBucket.delete(photoFilename);
+                            return res.status(500).json({
+                                success: false, error: 'Error when save the scaled image!'
+                            });
+                        }
+
+                        console.log('thumbnail', thumbnailPath, thumbnailFilename);
+
+                        // 5) Subimos el thumbnail de la imagen al storage
+                        fotosBucket.upload(thumbnailPath, function (err, file) {
+                            if (!err) {
+                                console.log('Error storing the thumbnail in Google Cloud Storage', err);
+                                fs.unlinkSync(photoPath);
+                                fs.unlinkSync(thumbnailPath);
+                                fotosBucket.delete(photoFilename);
+                                return res.status(500).json({success: false, error: err});
+                            }
+
+                            // 6) Generamos los nodos en LA BBDD de Firebase
+                            var photoKey = rootRef.child(nodePhotos).push().key;
+                            var imageData = {};
+                            imageData['url'] = efimerumStorageBucketPublicURL + '/' + req.files[0].filename;
+                            imageData['width'] = 0;
+                            imageData['height'] = 0;
+                            var thumbnailData = {};
+                            thumbnailData['url'] = efimerumStorageBucketPublicURL + '/' + req.files[0].filename;
+                            thumbnailData['width'] = 0;
+                            thumbnailData['height'] = 0;
+
+                            var now = moment();
+                            var photoData = {
+                                creationDate: now.unix(),
+                                expirationDate: now.add(1, 'hours').unix(),
+                                labels: labels,
+                                imageData: imageData,
+                                thumbnailData: thumbnailData,
+                                latitude: latitude,
+                                longitude: longitude,
+                                numOfLikes: 0
+                            };
+                            updates[nodePhotos + '/' + photoKey] = photoData;
+                            Object.keys(labelsEN).forEach(function (label) {
+                                updates[nodePhotosByLabels + '/' + languageEN + '/' + label + '/' + photoKey] = photoData;
+                            });
+                            Object.keys(labelsES).forEach(function (label) {
+                                updates[nodePhotosByLabels + '/' + languageES + '/' + label + '/' + photoKey] = photoData;
+                            });
+                            updates[nodePhotosPostedByUsers + '/' + uid + '/' + photoKey] = photoData;
+                            rootRef.update(updates)
+                                .then(function () {
+                                    fs.unlinkSync(photoPath);
+                                    fs.unlinkSync(thumbnailPath);
+                                    fotosBucket.delete(photoFilename);
+                                    fotosBucket.delete(thumbnailFilename);
+                                    return res.status(200).json({success: true, data: photoKey});
+                                })
+                                .catch(function (error) {
+                                    console.log('Error updating in Firebase', error);
+                                    fs.unlinkSync(photoPath);
+                                    fs.unlinkSync(thumbnailPath);
+                                    fotosBucket.delete(photoFilename);
+                                    fotosBucket.delete(thumbnailFilename);
+                                    return res.status(500).json({success: false, error: error});
+                                });
+                        });
+                    });
+                });
+            });
+        });
     });
 });
 
