09fefbcb76b805e488a710abb1ad6e35fe9c209f
Alberto Marín García <alberto.marin@gmail.com>
2017-02-14 16:27:32 +0100

    Formateo y se pasa a soportar solo los formatos de imágenes JPEG y PNG

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'ejs');
 app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,10 +29,10 @@ app.use('/api/v1/photos', require('./routes/api/v1/photos'));
 app.use('/api/v1/labels', require('./routes/api/v1/labels'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -40,25 +40,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: err,
-        status: err.status
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err,
+            status: err.status
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-      message: err.message,
-      error: {},
-      status: err.status
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {},
+        status: err.status
+    });
 });
 
 
